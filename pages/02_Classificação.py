0752cb0f16eea3d1aa0ff5d393249621e0ffd23a
Ronaldo Ribeiro <filhoronaldo.ribeiro@gmail.com>
2024-12-18 00:37:30 -0300

    Criei uma nova coluna para a Classificação
    Atualizei a EDA e o arquivo parquet

--- pages/02_Classificação.py
+++ pages/02_Classificação.py
@@ -1,12 +1,16 @@
 import numpy as np 
 import pandas as pd
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, GradientBoostingClassifier, VotingClassifier, StackingClassifier
+from xgboost import XGBClassifier
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
 
 import os
 for dirname, _, filenames in os.walk('/kaggle/input'):
     for filename in filenames:
         print(os.path.join(dirname, filename))
         
-
-file_path = '..\data\unistudents.parquet'
-df = pd.read_parquet(file_path)
-print(df.head(35))
+# Divide the dataframe into X and y
+X = df.drop('Change_Grades', axis=1)
+y = df['Change_Grades']
